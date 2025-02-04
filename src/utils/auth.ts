086da99835922e586fc6070791f3ea4dc30bd387
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-04 15:36:06 -0300

    feat: add auth.ts with sigIn method

---
+++ src/utils/auth.ts
@@ -0,0 +1,28 @@
+import { supabase } from "@/lib/supabaseClient";
+import { User } from "@/types/auth";
+
+export async function signIn(email: string, password: string): Promise<User> {
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      throw error.message;
+    }
+
+    if (!data || !data.user || !data.user.id || !data.user.email) {
+      throw "Invalid user data";
+    }
+
+    let res = {
+      id: data.user.id,
+      email: data.user.email,
+    };
+
+    return res;
+  } catch (error) {
+    throw error;
+  }
+}
