4fb71ce8e262431353ff2ad3fda87ba571186347
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-05 11:56:23 -0300

    refactor(dashboard): integrate Header component and remove sign-out button

--- src/app/pages/dashboard.tsx
+++ src/app/pages/dashboard.tsx
@@ -1,23 +1,13 @@
 "use client";
 
 import React from "react";
-import { signOut } from "@/utils/auth";
-import { Button } from "@/components";
+import { Header } from "@/components";
 
 export default function DashbparPage() {
-  async function handleSignOut() {
-    try {
-      console.log("signing out");
-      await signOut();
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
   return (
     <div>
-      <h1>Dashboard</h1>
-      <Button onClick={handleSignOut} label="Logout" />
+      <Header />
     </div>
   );
 }
