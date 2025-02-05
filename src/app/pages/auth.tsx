b4d07657d15578430414914e4ba1856aeb9f88b1
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-05 11:55:49 -0300

    chore(auth): import AuthForm from components index

--- src/app/pages/auth.tsx
+++ src/app/pages/auth.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import AuthForm from "@/components/AuthForm/AuthForm";
+import { AuthForm } from "@/components";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 
