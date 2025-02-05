a2b2d923c98744d179f6dc648d360b0443132448
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-05 10:22:24 -0300

    refactor(page): update LoginPage to conditionally render AuthPage or DashboardPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 "use client";
 
 import React from "react";
-import AuthForm from "@/components/AuthForm";
 import { useAuthStore } from "@/stores/authStore";
+import { AuthPage, DashboardPage } from "@/app/pages";
 
 export default function LoginPage() {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-
-  return (
-    <main className="min-h-screen flex flex-col md:flex-row">
-      <div className="hidden md:block md:flex-1 bg-almond-900">
-        {/* TODO: Inserir o logo ou imagem desejada */}
-      </div>
-      <div className="flex-1 flex flex-col items-center justify-center p-4">
-        <AuthForm/>
-      </div>
-    </main>
-  );
+  return isAuthenticated ? <DashboardPage /> : <AuthPage />
 }
