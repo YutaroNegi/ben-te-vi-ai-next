32a998dfbf608cf18345b86d053d9ce2d492ae9a
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-06 07:28:01 -0300

    feat(component): add InputDate e Select to index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,9 @@
 import AuthForm from "./AuthForm/AuthForm";
 import Button from "./Button/Button";
 import Input from "./Input/Input";
+import InputDate from "./InputDate/InputDate";
+import Select from "./Select/Select";
 import Header from "./Header/Header";
+import ExpenseForm from "./ExpenseForm/ExpenseForm";
 
-export { AuthForm, Button, Input, Header };
+export { AuthForm, Button, Input, Header, InputDate, Select, ExpenseForm };
