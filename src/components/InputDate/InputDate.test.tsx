87a3824114c771ce42c4b3358298ca84c592c796
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-06 07:26:48 -0300

    test(component): adicionar testes para o componente InputDate

---
+++ src/components/InputDate/InputDate.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import InputDate from './InputDate';
+
+describe('InputDate component', () => {
+  it('deve renderizar a label corretamente', () => {
+    render(<InputDate label="Selecione a data:" />);
+    expect(screen.getByText('Selecione a data:')).toBeDefined();
+  });
+
+
+  it('deve aplicar atributos adicionais no input', () => {
+    render(<InputDate aria-label="input-date" name="data" />);
+    const inputElement = screen.getByLabelText('input-date');
+    expect(inputElement).toBeDefined();
+  });
+});
