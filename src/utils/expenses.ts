b718737fcbfbcd944645ace9fb2fd8b53605badc
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-06 08:13:02 -0300

    feat(utils): add registerExpense function for expense registration

--- src/utils/expenses.ts
+++ src/utils/expenses.ts
@@ -1,6 +1,5 @@
 import { CategoryOption } from "@/types";
 
-
 export async function fetchCategories(userId: string) {
   try {
     const res = await fetch(`/api/categories/user/${userId}`);
@@ -19,3 +18,33 @@ export async function fetchCategories(userId: string) {
     return optionsData;
   }
 }
+
+interface ExpenseData {
+  user_id: string;
+  name: string;
+  amount: number;
+  category_id: string
+  description: string | null;
+  date: string;
+  installments: number;
+}
+
+export const registerExpense = async (expenseData: ExpenseData) => {
+  try {
+    const res = await fetch("/api/expenses/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(expenseData),
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(data.error || "Failed to register expense");
+    }
+  } catch (error: any) {
+    throw error;
+  }
+};
