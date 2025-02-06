3d3bcaf9a347c2e982c23499544cc077d5c6af02
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-06 09:11:07 -0300

    feat(types): export Category interface for expense categorization

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,10 +1,11 @@
 import { User, AuthState } from './auth'
-import { CategoryOption, ExpenseData, Installment } from './expenses'
+import { CategoryOption, ExpenseData, Installment, Category } from './expenses'
 
 export type {
   User,
   AuthState,
   CategoryOption,
   ExpenseData,
-  Installment
+  Installment,
+  Category
 }
