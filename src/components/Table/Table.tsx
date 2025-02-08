938c30086bf625ed44e0540d4f84a8e1cb02f9ba
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-08 20:06:45 -0300

    style(table): smaller font size

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -11,14 +11,14 @@ interface TableProps {
 const Table: React.FC<TableProps> = ({ title, headers, rows }) => {
   return (
     <div className="my-4 shadow-lg rounded">
-      <h2 className="bg-matcha-900 text-white text-xl font-bold p-4">
+      <h2 className="bg-matcha-900 text-white text-base font-bold p-2">
         {title}
       </h2>
       <table className="min-w-full border-collapse">
         <thead className="bg-matcha-800">
           <tr>
             {headers.map((header, index) => (
-              <th key={index} className="border p-2 text-white">
+              <th key={index} className="border p-1 text-white text-sm">
                 {header}
               </th>
             ))}
@@ -28,7 +28,7 @@ const Table: React.FC<TableProps> = ({ title, headers, rows }) => {
           {rows.map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((cell, cellIndex) => (
-                <td key={cellIndex} className="border p-2 text-black">
+                <td key={cellIndex} className="border p-1 text-black text-sm">
                   {cell}
                 </td>
               ))}
