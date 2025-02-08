344e5156557db6564855cb531a17d128d35829a2
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-08 20:29:44 -0300

    feat(InputDate): better label

--- src/components/InputDate/InputDate.tsx
+++ src/components/InputDate/InputDate.tsx
@@ -6,11 +6,20 @@ interface InputDateProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 const InputDate: React.FC<InputDateProps> = ({ label, ...props }) => {
   return (
-    <div className="flex flex-col space-y-1">
-      {label && <label className="font-medium text-gray-700">{label}</label>}
+    <div className="flex items-stretch w-64 rounded-full overflow-hidden border border-gray-300">
+      {label && (
+        <span
+          className="flex items-center justify-center bg-matcha-900 text-white px-4"
+          style={{ width: "30%" }}
+        >
+          {label}
+        </span>
+      )}
       <input
         type="date"
-        className="rounded border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-64 rounded-full"
+        className={`px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black ${
+          label ? "w-[70%]" : "w-full"
+        }`}
         {...props}
       />
     </div>
