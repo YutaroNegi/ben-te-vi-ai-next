a84c22bdace36bfee94ee312fdf6f4da4bad88ed
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-09 20:17:24 -0300

    style(Table.tsx): fixed td row width

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -18,7 +18,7 @@ const Table: React.FC<TableProps> = ({ title, headers, rows }) => {
         <thead className="bg-matcha-800">
           <tr>
             {headers.map((header, index) => (
-              <th key={index} className="p-2 text-white text-sm min-w-[50px] text-center">
+              <th key={index} className="p-2 text-white text-xs min-w-[80px] max-w-[80px] text-center">
                 {header}
               </th>
             ))}
@@ -28,7 +28,7 @@ const Table: React.FC<TableProps> = ({ title, headers, rows }) => {
           {rows.map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((cell, cellIndex) => (
-                <td key={cellIndex} className="border p-2 text-black text-sm min-w-[50px] text-center">
+                <td key={cellIndex} className="border p-2 text-black text-xs min-w-[80px] max-w-[80px] text-center">
                   {cell}
                 </td>
               ))}
