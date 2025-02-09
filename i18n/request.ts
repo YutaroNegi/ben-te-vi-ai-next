42026cdfc83ed73e9f7091baf0addb662c4b9b57
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-09 09:26:21 -0300

    style(request.ts): add auto detect lang for en and ptbr

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,24 @@
+import { headers } from "next/headers";
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = "en";
+  // Valor padrão caso não seja possível detectar o idioma
+  let locale = "en";
+
+  // Usa a função headers() para obter os cabeçalhos da requisição
+  const headerList = await headers();
+  const acceptLanguage = headerList.get("accept-language");
+
+  if (acceptLanguage) {
+    const firstLang = acceptLanguage.split(",")[0].trim().toLowerCase();
+
+    if (firstLang.startsWith("pt")) {
+      locale = "ptBR";
+    }
+    else if (firstLang.startsWith("en")) {
+      locale = "en";
+    }
+  }
 
   return {
     locale,
