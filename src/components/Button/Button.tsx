3b87a32577d830e8369d31f6b808bfdbe4be741c
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-11 11:17:23 -0300

    refactor(Button): replace loading image with LoadingSpinner component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import { LoadingSpinner } from "@/components";
 
 interface ButtonProps {
   label: string;
@@ -23,17 +23,7 @@ const Button: React.FC<ButtonProps> = ({
       disabled={loading}
       className={`flex items-center justify-center px-4 py-2 bg-bentenavi-900 hover:bg-bentenavi-900 text-white font-medium rounded-full transition-colors duration-200 ${className} w-32`}
     >
-      {loading ? (
-        <Image
-          src="/loading.svg"
-          alt="Loading"
-          width={20}
-          height={20}
-          className="h-5 w-5 animate-spin"
-        />
-      ) : (
-        label
-      )}
+      {loading ? <LoadingSpinner /> : label}
     </button>
   );
 };
