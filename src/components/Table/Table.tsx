d771737178c304b3510a093aa5f9b8f58500f895
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-11 09:16:41 -0300

    style(Table): rounded tables

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -16,18 +16,21 @@ const Table: React.FC<TableProps> = ({
   columnWidths,
 }) => {
   return (
-    <div className="my-4 shadow-lg rounded">
+    // Adicionamos overflow-hidden para garantir que os cantos arredondados não sejam “vazados”
+    <div className="my-4 shadow-lg rounded overflow-hidden">
       <h2 className="bg-matcha-900 text-white text-sm font-bold p-2">
         {title}
       </h2>
-      <table className="min-w-full border-collapse">
+      <table className="min-w-full border-separate border-spacing-0">
         <thead className="bg-matcha-800">
           <tr>
             {headers.map((header, index) => (
               <th
                 key={index}
                 style={{ width: columnWidths?.[index] }}
-                className="p-2 text-white text-xs text-center"
+                className={`
+                  p-2 text-white text-xs text-center
+                `}
               >
                 {header}
               </th>
@@ -41,7 +44,11 @@ const Table: React.FC<TableProps> = ({
                 <td
                   key={cellIndex}
                   style={{ width: columnWidths?.[cellIndex] }}
-                  className="border p-2 text-black text-xs text-clip"
+                  className={`
+                    border p-2 text-black text-xs text-clip
+                    ${rowIndex === rows.length - 1 && cellIndex === 0 ? "rounded-bl-lg" : ""}
+                    ${rowIndex === rows.length - 1 && cellIndex === row.length - 1 ? "rounded-br-lg" : ""}
+                  `}
                 >
                   {cell}
                 </td>
