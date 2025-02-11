2243caec0e42e988727b414cc35dfe34aff3161a
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-11 07:55:41 -0300

    feat: add toast container in the root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,14 @@
 import React from "react";
 import { useAuthStore } from "@/stores/authStore";
 import { AuthPage, DashboardPage } from "@/app/pages";
+import { CustomToast } from "@/components";
 
 export default function LoginPage() {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  return isAuthenticated ? <DashboardPage /> : <AuthPage />
+  return (
+    <>
+      {isAuthenticated ? <DashboardPage /> : <AuthPage />}
+      <CustomToast />
+    </>
+  );
 }
