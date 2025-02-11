bc2dff97b96c018d9ed231dcd07cdb65d71a716d
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-11 18:33:04 -0300

    feat(DashboardPage): integrate ExpensePieChart and ExpensesBarChart components into layout

--- src/app/pages/dashboard.tsx
+++ src/app/pages/dashboard.tsx
@@ -1,14 +1,28 @@
 "use client";
 
 import React from "react";
-import { Header, ExpenseForm, ViewExpenses } from "@/components";
+import {
+  Header,
+  ExpenseForm,
+  ViewExpenses,
+  ExpensePieChart,
+  ExpensesBarChart
+} from "@/components";
 
-export default function DashbparPage() {
+export default function DashboardPage() {
   return (
     <div>
       <Header />
-      <div className="flex justify-center mt-5">
-        <ExpenseForm />
+      <div className="flex flex-col md:flex-row justify-center mt-5 space-y-5 md:space-y-0 md:space-x-5">
+        <div className="w-full md:w-1/3 p-2">
+          <ExpensesBarChart />
+        </div>
+        <div className="w-full md:w-1/3 p-2">
+          <ExpenseForm />
+        </div>
+        <div className="w-full md:w-1/3 p-2">
+          <ExpensePieChart />
+        </div>
       </div>
       <div className="flex justify-center mt-5">
         <ViewExpenses />
