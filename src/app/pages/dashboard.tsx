17d186073c3786a9723215fee6629d6b3bc3407a
Yutaro Negi <souza_yutaro@hotmail.com>
2025-02-25 08:18:33 -0300

    style(DashboardPage): add bords to components

--- src/app/pages/dashboard.tsx
+++ src/app/pages/dashboard.tsx
@@ -15,13 +15,21 @@ export default function DashboardPage() {
       <Header />
       <div className="flex flex-col md:flex-row justify-center mt-2 space-y-5 md:space-y-0 md:space-x-5">
         <div className="w-full md:w-1/3 p-2">
-          <ExpensesBarChart />
+          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[400px] flex items-center justify-center">
+            <ExpensesBarChart />
+          </div>
         </div>
+
         <div className="w-full md:w-1/3 p-2">
-          <ExpenseForm />
+          <div className="border-2 border-chocolate-800 rounded-lg shadow p-4 h-[400px] flex items-center justify-center">
+            <ExpenseForm />
+          </div>
         </div>
+
         <div className="w-full md:w-1/3 p-2">
-          <ExpensePieChart />
+          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[400px] flex items-center justify-center">
+            <ExpensePieChart />
+          </div>
         </div>
       </div>
       <div className="flex justify-center mt-5">
