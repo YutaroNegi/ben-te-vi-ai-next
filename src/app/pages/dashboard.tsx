6c4ef942d69f29c4059975d7a88b19316ab3d535
Yutaro Negi <souza_yutaro@hotmail.com>
2025-03-02 07:54:46 -0300

    style(DashboardPage): decrease top cards height

--- src/app/pages/dashboard.tsx
+++ src/app/pages/dashboard.tsx
@@ -15,19 +15,19 @@ export default function DashboardPage() {
       <Header />
       <div className="flex flex-col md:flex-row justify-center mt-2 space-y-5 md:space-y-0 md:space-x-5">
         <div className="w-full md:w-1/3 p-2">
-          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[400px] flex items-center justify-center">
+          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[320px] flex items-center justify-center">
             <ExpensesBarChart />
           </div>
         </div>
 
         <div className="w-full md:w-1/3 p-2">
-          <div className="border-2 border-chocolate-800 rounded-lg shadow p-4 h-[400px] flex items-center justify-center">
+          <div className="border-2 border-chocolate-800 rounded-lg shadow p-4 h-[320px] flex items-center justify-center">
             <ExpenseForm />
           </div>
         </div>
 
         <div className="w-full md:w-1/3 p-2">
-          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[400px] flex items-center justify-center">
+          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[320px] flex items-center justify-center">
             <ExpensePieChart />
           </div>
         </div>
