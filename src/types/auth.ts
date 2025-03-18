092ae757b02f6dbb216c295c51699bdc76f3932b
Yutaro Negi <souza_yutaro@hotmail.com>
2025-03-18 11:00:57 -0300

    feat(auth): add lastLogin property to User and AuthState types

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,6 +1,7 @@
 export type User = {
   id: string;
   email: string;
+  lastLogin: string | undefined;
 };
 
 export type AuthState = {
@@ -8,4 +9,5 @@ export type AuthState = {
   isAuthenticated: boolean;
   login: (user: User) => void;
   logout: () => void;
+  lastLogin: string | null;
 };
