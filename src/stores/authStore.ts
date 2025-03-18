38a26790517e46b02105af4913e2135b0aaa1c5d
Yutaro Negi <souza_yutaro@hotmail.com>
2025-03-18 11:00:50 -0300

    feat(authStore): add lastLogin to track user's last login time

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -4,6 +4,7 @@ import { AuthState } from "@/types/auth";
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   isAuthenticated: false,
-  login: (user) => set({ user, isAuthenticated: true }),
+  login: (user) => set({ user, isAuthenticated: true, lastLogin: user.lastLogin }),
   logout: () => set({ user: null, isAuthenticated: false }),
+  lastLogin: null,
 }));
