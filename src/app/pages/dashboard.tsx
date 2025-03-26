cb422552e6204cb48e898dc674f77d48dac1e576
Yutaro Negi <souza_yutaro@hotmail.com>
2025-03-26 05:27:09 -0300

    feat(dashboard): add translations for barChart and pieChart titles

--- src/app/pages/dashboard.tsx
+++ src/app/pages/dashboard.tsx
@@ -8,26 +8,43 @@ import {
   ExpensePieChart,
   ExpensesBarChart,
 } from "@/components";
+import { useTranslations } from "next-intl";
+import { useExpensesStore } from "@/stores/expenseStore";
 
 export default function DashboardPage() {
+  const t = useTranslations("Dashboard");
+  const { selectedDate } = useExpensesStore();
+  const selectedMonth = selectedDate
+    .toLocaleString("default", {
+      month: "long",
+    })
+    .toUpperCase();
+
   return (
     <div>
       <Header />
       <div className="flex flex-col md:flex-row justify-center mt-2 space-y-5 md:space-y-0 md:space-x-5">
         <div className="w-full md:w-1/3 p-2">
-          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[320px] flex items-center justify-center">
+          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[360px] flex items-center justify-center flex-col justify-between">
+            <h3 className="text-chocolate-950 font-bold">
+              {t("barChart").toUpperCase()}
+            </h3>
             <ExpensesBarChart />
           </div>
         </div>
 
         <div className="w-full md:w-1/3 p-2">
-          <div className="border-2 border-chocolate-800 rounded-lg shadow p-4 h-[320px] flex items-center justify-center">
+          <div className="border-2 border-chocolate-800 rounded-lg shadow p-4 h-[360px] flex items-center justify-center flex-col">
+            <h3 className="text-chocolate-950 font-bold">{selectedMonth}</h3>
             <ExpenseForm />
           </div>
         </div>
 
         <div className="w-full md:w-1/3 p-2">
-          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[320px] flex items-center justify-center">
+          <div className=" border-2 border-chocolate-800 rounded-lg shadow p-4 h-[360px] flex items-center justify-center flex-col justify-between">
+            <h3 className="text-chocolate-950 font-bold">
+              {t("pieChart").toUpperCase()}
+            </h3>
             <ExpensePieChart />
           </div>
         </div>
