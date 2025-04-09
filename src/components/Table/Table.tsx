048147be0237f6aeb72336f06d4a77cff0f025b8
Yutaro Negi <souza_yutaro@hotmail.com>
2025-04-09 05:06:19 -0300

    style(table): center header title

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -41,7 +41,7 @@ const Table: React.FC<TableProps> = ({
   return (
     // Adicionamos overflow-hidden para garantir que os cantos arredondados não sejam “vazados”
     <div className="my-4 shadow-lg rounded mb-[50px]">
-      <h2 className="bg-matcha-light text-white text-sm font-bold p-2">
+      <h2 className="bg-matcha-light text-white text-sm font-bold p-2 text-center">
         {title}
       </h2>
       <table className="min-w-full border-separate border-spacing-0">
