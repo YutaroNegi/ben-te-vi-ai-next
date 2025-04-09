d0a170fb3ea8bba2f5f877ac4fcd5d728170ba90
Yutaro Negi <souza_yutaro@hotmail.com>
2025-04-09 05:24:47 -0300

    style(CustomDropdown): update text colors and sizes for edit and delete options

--- src/components/CustomDropdown/CustomDropdown.tsx
+++ src/components/CustomDropdown/CustomDropdown.tsx
@@ -255,18 +255,16 @@ const CustomDropdown: React.FC<CustomDropdownProps> = ({
 
                 <div className="hidden group-hover:flex flex-col absolute top-0 left-full ml-2 bg-white border border-gray-200 rounded shadow-lg z-20">
                   <div
-                    className="cursor-pointer flex items-center px-4 py-2 text-xs text-gray-700 hover:bg-gray-100"
+                    className="cursor-pointer flex items-center px-4 py-2 text-xs text-matcha-dark hover:bg-gray-100"
                     onClick={() => handleStartEdit(option)}
                   >
-                    <FaEdit className="mr-2" />
-                    {t("edit")}
+                    <FaEdit className="text-[17px]" />
                   </div>
                   <div
-                    className="cursor-pointer flex items-center px-4 py-2 text-xs text-red-600 hover:bg-gray-100"
+                    className="cursor-pointer flex items-center px-4 py-2 text-xs text-bentenavi-dark hover:bg-gray-100"
                     onClick={() => handleDeleteClick(option)}
                   >
-                    <FaTrash className="mr-2" />
-                    {t("delete")}
+                    <FaTrash className="text-[15px]" />
                   </div>
                 </div>
               </div>
