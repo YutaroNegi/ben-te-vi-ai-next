be6cc505c594bd2f1837fbfc9d830ffc2b7addca
Yutaro Negi <souza_yutaro@hotmail.com>
2025-05-02 07:59:56 -0300

    feat: add  ExpenseType to category store

--- src/stores/categoryStore.ts
+++ src/stores/categoryStore.ts
@@ -7,14 +7,22 @@ import {
   editCategory,
   deleteCategory,
 } from "@/utils";
-import { CategoryOption } from "@/types";
+import { CategoryOption, ExpenseType } from "@/types";
 
 interface CategoryStoreState {
   categories: CategoryOption[];
   loadingCategories: boolean;
-  fetchCategoriesForUser: (userId: string) => Promise<void>;
-  addCategory: (userId: string, name: string) => Promise<void>;
-  updateCategory: (categoryId: string, newName: string) => Promise<void>;
+  fetchCategoriesForUser: (userId: string, type: ExpenseType) => Promise<void>;
+  addCategory: (
+    userId: string,
+    name: string,
+    type: ExpenseType,
+  ) => Promise<void>;
+  updateCategory: (
+    categoryId: string,
+    newName: string,
+    type: ExpenseType,
+  ) => Promise<void>;
   removeCategory: (categoryId: string) => Promise<void>;
   clearCategories: () => void;
 }
@@ -26,10 +34,10 @@ export const useCategoryStore = create<CategoryStoreState>((set, get) => ({
   /**
    * Busca todas as categorias do usuário
    */
-  fetchCategoriesForUser: async (userId: string) => {
+  fetchCategoriesForUser: async (userId: string, type: ExpenseType) => {
     set({ loadingCategories: true });
     try {
-      const categories = await fetchCategories(userId);
+      const categories = await fetchCategories(userId, type);
       set({ categories });
     } catch (error) {
       console.error(error);
@@ -42,16 +50,16 @@ export const useCategoryStore = create<CategoryStoreState>((set, get) => ({
   /**
    * Adiciona nova categoria e refaz o fetch
    */
-  addCategory: async (userId: string, name: string) => {
+  addCategory: async (userId: string, name: string, type: ExpenseType) => {
     if (!name) return;
 
     set({ loadingCategories: true });
     try {
-      await registerCategory({ user_id: userId, name, description: "" });
+      await registerCategory({ user_id: userId, name, description: "", type });
       toast.success("Categoria adicionada com sucesso!");
 
       // Atualiza a lista de categorias após inserir
-      await get().fetchCategoriesForUser(userId);
+      await get().fetchCategoriesForUser(userId, type);
     } catch (error) {
       console.error(error);
       toast.error("Falha ao adicionar categoria");
@@ -63,12 +71,16 @@ export const useCategoryStore = create<CategoryStoreState>((set, get) => ({
   /**
    * Edita uma categoria e refaz o fetch
    */
-  updateCategory: async (categoryId: string, newName: string) => {
+  updateCategory: async (
+    categoryId: string,
+    newName: string,
+    type: ExpenseType,
+  ) => {
     if (!newName) return;
 
     set({ loadingCategories: true });
     try {
-      await editCategory(categoryId, { name: newName, description: "" });
+      await editCategory(categoryId, { name: newName, description: "", type });
       toast.success("Category successfully edited!");
     } catch (error) {
       console.error(error);
