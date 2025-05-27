2c7bfb500334512767b893c9928e4ae660b5067a
Yutaro Negi <souza_yutaro@hotmail.com>
2025-05-27 08:50:16 -0300

    feat(pluggy): add Connector interface and ItemData type

---
+++ src/types/pluggy.ts
@@ -0,0 +1,55 @@
+interface Connector {
+  id: number;
+  name: string;
+  primaryColor: string;
+  institutionUrl: string;
+  country: string;
+  type: string;
+  credentials: {
+    validation: string;
+    validationMessage: string;
+    label: string;
+    name: string;
+    type: string;
+    placeholder: string;
+    optional: boolean;
+  }[];
+  imageUrl: string;
+  hasMFA: boolean;
+  oauth: boolean;
+  health: {
+    status: string;
+    stage: string | null;
+  };
+  products: string[];
+  createdAt: string;
+  isSandbox: boolean;
+  isOpenFinance: boolean;
+  updatedAt: string;
+  supportsPaymentInitiation: boolean;
+  supportsScheduledPayments: boolean;
+  supportsSmartTransfers: boolean;
+  supportsBoletoManagement: boolean;
+}
+
+export interface ItemData {
+  id: string;
+  connector: Connector;
+  createdAt: string;
+  updatedAt: string;
+  status: string;
+  executionStatus: string;
+  lastUpdatedAt: string;
+  webhookUrl: string | null;
+  error: string | null;
+  clientUserId: string | null;
+  consecutiveFailedLoginAttempts: number;
+  statusDetail: string | null;
+  parameter: string | null;
+  userAction: string | null;
+  nextAutoSyncAt: string | null;
+  autoSyncDisabledAt: string | null;
+  consentExpiresAt: string | null;
+  products: string[];
+  oauthRedirectUri: string | null;
+}
