027d39a58b6d7e57f1cc670c46318562c69441e9
Yutaro Negi <souza_yutaro@hotmail.com>
2025-05-28 10:21:52 -0300

    feat(transactions): implement action handler for transaction selection and modal display

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -4,14 +4,18 @@ import React, { useState, useEffect } from "react";
 import { useTranslations } from "next-intl";
 import { Table } from "@/components";
 import { MdAdd } from "react-icons/md";
-import { Transaction } from "@/types";
+import { Transaction, InitialExpenseValues } from "@/types";
 
 interface TransactionsTableProps {
   transactions: Transaction[];
+  setInitialValue?: (value: InitialExpenseValues) => void;
+  setShowModal: (show: boolean) => void;
 }
 
 const TransactionsTable: React.FC<TransactionsTableProps> = ({
   transactions,
+  setInitialValue,
+  setShowModal,
 }) => {
   const t = useTranslations("TransactionsTable");
   const [openMenuId, setOpenMenuId] = useState<string | null>(null);
@@ -39,21 +43,49 @@ const TransactionsTable: React.FC<TransactionsTableProps> = ({
     t("headers.actions"),
   ];
 
+  const handleActionClick = (tx: Transaction) => {
+    if (tx.imported) return;
+    const initialValue: InitialExpenseValues = {
+      type: "expense",
+      name: tx.description,
+      description: tx.description,
+      created_at: new Date(tx.date).toISOString().split("T")[0],
+      amount: tx.amount,
+      installments: 1,
+      pluggy_transaction_id: tx.id,
+    };
+    console.log("Initial Value:", initialValue);
+    if (setInitialValue) {
+      setInitialValue(initialValue);
+      setShowModal(true);
+    }
+    setOpenMenuId(null);
+  };
+
   const rows = transactions.map((tx) => [
     tx.category,
     tx.description,
-    tx.date,
+    new Date(tx.date).toLocaleString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }),
     tx.amount.toLocaleString("pt-BR", {
       style: "currency",
       currency: "BRL",
     }),
-    <div key="actions" className="grid place-items-center cursor-pointer">
+    <button
+      key="actions"
+      className="grid place-items-center cursor-pointer"
+      onClick={() => handleActionClick(tx)}
+    >
       <MdAdd color={tx.imported ? "grey" : "green"} />
-    </div>,
+    </button>,
   ]);
 
   return (
     <Table
+      className="w-[80vw] mx-auto"
       title={t("title")}
       headers={headers}
       rows={rows}
