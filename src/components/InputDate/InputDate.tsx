8794eb6e4e131c3a7ec0e9e851a3bbefde306dcc
Yutaro Negi <souza_yutaro@hotmail.com>
2025-05-28 10:20:04 -0300

    feat(input-date): add initialValue prop for default date value

--- src/components/InputDate/InputDate.tsx
+++ src/components/InputDate/InputDate.tsx
@@ -3,11 +3,13 @@ import React from "react";
 interface InputDateProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   labelClassName?: string;
+  initialValue?: string;
 }
 
 const InputDate: React.FC<InputDateProps> = ({
   label,
   labelClassName = "bg-matcha-dark",
+  initialValue,
   ...props
 }) => {
   return (
@@ -25,6 +27,7 @@ const InputDate: React.FC<InputDateProps> = ({
         className={`px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black ${
           label ? "w-[70%]" : "w-full"
         }`}
+        defaultValue={initialValue}
         {...props}
       />
     </div>
